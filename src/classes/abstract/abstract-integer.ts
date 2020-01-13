5e31ba678bdd0369db1385f4ed28ee9ce1c549f3
Kernichnyy Andrey <only.exlight@gmail.com>
2020-01-14 01:49:15 +0700

    Add methods to Interface/Abstract class

--- src/classes/abstract/abstract-integer.ts
+++ src/classes/abstract/abstract-integer.ts
@@ -60,20 +60,53 @@ export abstract class AbstractIntType implements INumberType {
     return this._value.toString();
   }
 
+  // ---Mathematics---
+
   public abstract add(term: INumberType | number): AbstractIntType;
   public abstract subtract(subtrahend: INumberType | number): AbstractIntType;
   public abstract multiply(multiplier: INumberType | number): AbstractIntType;
   public abstract devide(devider: INumberType | number): Float;
   public abstract pow(exponent: INumberType | number): AbstractIntType;
+  public abstract mod(arg: INumberType | number): AbstractIntType;
+
+  // ---Increments/Dicrements---
+
+  public abstract inc(): AbstractIntType;
+  public abstract dec(): AbstractIntType;
+
+  // ---Shifts---
 
   public abstract shiftRight(posCount: number): AbstractIntType;
   public abstract shiftLeft(posCount: number): AbstractIntType;
 
+  // ---Binary---
+
+  public abstract binOr(arg: INumberType | number): AbstractIntType;
+  public abstract binAnd(arg: INumberType | number): AbstractIntType;
+  public abstract binNot(): AbstractIntType;
+  public abstract xor(arg: INumberType | number): AbstractIntType;
+
+  // ---Logic---
+
   public abstract or(arg: INumberType | number): AbstractIntType;
   public abstract and(arg: INumberType | number): AbstractIntType;
 
-  public abstract inc(): AbstractIntType;
-  public abstract dec(): AbstractIntType;
+  public not(): boolean {
+    return !this._value;
+  }
+
+  // ---Equality---
+
+  public equal(arg: INumberType | number): boolean {
+    return arg.valueOf() === this._value;
+  }
+
+  public notEqual(arg: INumberType | number): boolean {
+    return arg.valueOf() !== this._value;
+  }
+
+  public abstract tEqual(arg: INumberType | number): boolean;
+  public abstract tNotEqual(arg: INumberType | number): boolean;
 
   public more(arg: INumberType | number): boolean {
     const val = arg.valueOf ? arg.valueOf() : arg;
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -9,20 +9,44 @@ export interface INumberType extends Number {
   valueOf(): number;
   toString(): string;
 
+  // ---Mathematics---
+
   add(term: INumberType | number): AbstractIntType;
   subtract(subtrahend: INumberType | number): AbstractIntType;
   multiply(multiplier: INumberType | number): AbstractIntType;
   devide(devider: INumberType | number): Float;
+  mod(devider: INumberType | number): AbstractIntType;
   pow(exponent: INumberType | number): AbstractIntType;
+
+  // ---Increments/Dicrements---
 
   inc(): AbstractIntType;
   dec(): AbstractIntType;
 
+  // ---Shifts---
+
   shiftLeft(posCount: number): AbstractIntType;
   shiftRight(posCount: number): AbstractIntType;
 
+  // ---Binary---
+
+  binOr(arg: INumberType | number): AbstractIntType;
+  binAnd(arg: INumberType | number): AbstractIntType;
+  binNot(): AbstractIntType;
+  xor(arg: INumberType | number): AbstractIntType;
+
+  // ---Logic---
+
   or(arg: INumberType | number): AbstractIntType;
   and(arg: INumberType | number): AbstractIntType;
+  not(): boolean;
+
+  // ---Equality---
+
+  equal(exponent: INumberType | number): boolean;
+  tEqual(exponent: INumberType | number): boolean;
+  notEqual(exponent: INumberType | number): boolean;
+  tNotEqual(exponent: INumberType | number): boolean;
 
   more(arg: INumberType | number): boolean;
   moreOrEqual(arg: INumberType | number): boolean;
